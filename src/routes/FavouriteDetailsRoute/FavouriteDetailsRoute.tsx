166d82d1599f8bfc8332febd3939349618dcd233
KrauzinDaniil <krauzin1995@gmail.com>
2024-04-09 23:21:50 +0600

    fixes

--- src/routes/FavouriteDetailsRoute/FavouriteDetailsRoute.tsx
+++ src/routes/FavouriteDetailsRoute/FavouriteDetailsRoute.tsx
@@ -4,22 +4,24 @@ import Description from "../../components/Description/Description";
 import { DescriptionProps } from "../../types/descriptionProps";
 
 const FavouriteDetailsRoute: React.FC = () => {
-    const [description, setDescription] = useState<DescriptionProps | null>(null);
+  const [description, setDescription] = useState<DescriptionProps | null>(null);
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
-        const storedDescription = localStorage.getItem(Number(id).toString());
-        if (storedDescription) {
-            
-            setDescription(JSON.parse(storedDescription));
-        }
+  useEffect(() => {
+    const storedDescription = localStorage.getItem(Number(id).toString());
+    if (storedDescription) {
+      setDescription(JSON.parse(storedDescription));
+    }
+  }, [id]);
 
-    }, [id]);
-    
   return (
     <div>
-    { description !== null ? <Description description={description} isFavourited ={true}/> : "" } 
+      {description !== null ? (
+        <Description description={description} isFavourited={true} />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
