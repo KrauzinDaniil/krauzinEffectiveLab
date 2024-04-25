ada3576eba004428f2d98b193f7a8056d5aec47b
KrauzinDaniil <krauzin1995@gmail.com>
2024-04-25 10:24:50 +0600

    character virtualisation added

--- src/components/Display/DisplayList.tsx
+++ src/components/Display/DisplayList.tsx
@@ -2,6 +2,7 @@ import classes from "./DisplayList.module.css";
 import Search from "./Search/Search";
 import Card from "../Card/Card";
 import { DisplayInterface } from "../../types/DisplayInterface";
+import {VirtuosoGrid } from "react-virtuoso";
 
 
 const DisplayList: React.FC<{
@@ -9,9 +10,12 @@ const DisplayList: React.FC<{
   type: boolean;
   onSetSearch: (value: string) => void;
   changeLocalStorage: (key: string, value: string, mode:string) => void;
+  fetchMoreData: () => void;
   loading: boolean;
   total:number;
-}> = ({ display, type, onSetSearch, loading, total, changeLocalStorage,  }) => {
+}> = ({ display, type, onSetSearch, loading, total, changeLocalStorage, fetchMoreData  }) => {
+
+  
   return (
     <main className={classes.main}>
       <Search
@@ -20,33 +24,43 @@ const DisplayList: React.FC<{
         total = {total}
       ></Search>
 
-      {loading ? (
-        <div className={classes.loadingImage}>
+     
+       
+      
+        
+         <div className={classes.divider}></div>
+
+            
+          <VirtuosoGrid
+              useWindowScroll={true} 
+              data={display}
+              className={classes.list}
+              listClassName ={classes.rowList}
+              itemClassName ={classes.itemRow}
+              endReached={fetchMoreData}
+              itemContent={(_, user) =>
+                  <Card
+                    key={user.id}
+                    thumbnail={user.thumbnail?.path}
+                    name={user.name}
+                    description={user.description}
+                    id={user.id}
+                    isHero={user.isChar}
+                    changeStorage={changeLocalStorage}
+                    isFavourited={false}
+                    dataList={user.data}
+                  />
+                
+              }
+           />
+          { loading ? ( <div className={classes.loadingImage}>
           <img src="/public/marvelous.png" alt="" />
           <div className={classes.loadingLabel}> Please wait</div>
-        </div>
-      ) : (
-        <>
-         <div className={classes.divider}></div>
-          <div className={classes.list}>
-            {display.map((item) => (
-              <Card
-                key={item.id}
-                thumbnail={item.thumbnail?.path}
-                name={item.name}
-                description={item.description}
-                id={item.id}
-                isHero={item.isChar}
-                changeStorage={changeLocalStorage}
-                isFavourited ={false}
-                dataList={item.data}
-
-              />
-            ))}
-          </div>
-          <div className={classes.divider}></div>
-        </>
-      )}
+        </div>)  : "" }
+           
+      
+      
+      
     </main>
   );
 };
--- src/routes/Characters/CharactersRoute.tsx
+++ src/routes/Characters/CharactersRoute.tsx
@@ -2,20 +2,12 @@ import { useEffect, FC, useState } from "react";
 import { observer } from "mobx-react-lite";
 import Display from "../../components/Display";
 import characterStore from "../../stores/Characters";
-import classes from "./CharactersRoute.module.css";
 import { useDebounce } from "../../components/Hooks/debounce";
 import { ToastContainer } from "react-toastify";
-import { AiOutlineDoubleRight } from "react-icons/ai";
-import { AiOutlineDoubleLeft } from "react-icons/ai";
-import { AiOutlineLeft } from "react-icons/ai";
-import { AiOutlineRight } from "react-icons/ai";
+
 
 const CharactersRoute: FC = () => {
   const { characters, loading } = characterStore;
-
-  const [lowBorder, setLowBorder] = useState(1);
-
-  const [highBorder, setHighBorder] = useState(5);
 
   const [toSearch, setToSearch] = useState("");
 
@@ -44,28 +36,15 @@ const CharactersRoute: FC = () => {
     return null;
   }
 
-  const comp = [];
-  for (let i = lowBorder; i < highBorder + 1; i++) {
-    comp.push(
-      <div
-        onClick={() => {
-          loadAdditionalPage((i - 1) * 25);
-        }}
-        className={
-          i === characterStore.currentPage ? classes.cellActive : classes.cell
-        }
-      >
-        {i}
-      </div>
-    );
-  }
+
 
   useEffect(() => {
     characterStore.getCharacterList(debounceSearchedItem);
   }, [debounceSearchedItem]);
 
-  function loadAdditionalPage(offset: number) {
-    characterStore.getCharacterListWithOffset(offset, toSearch);
+
+  function fetchMoreData(): void  {
+         characterStore.getCharacterListWithOffset(toSearch)
   }
 
   return (
@@ -78,69 +57,10 @@ const CharactersRoute: FC = () => {
         loading={loading}
         total={characterStore.totalCharacters}
         changeLocalStorage={changeLocalStorage}
+        fetchMoreData={fetchMoreData}
       />
 
-      {!loading && (
-        <div className={classes.buttonPanel}>
-          <div
-            className={classes.navButton}
-            onClick={() => {
-              if (lowBorder > 5) {
-                setHighBorder(highBorder - 5);
-                setLowBorder(lowBorder - 5);
-              }
-            }}
-          >
-            {lowBorder > 5 ? <AiOutlineDoubleLeft /> : ""}
-          </div>
-
-          <div
-            className={classes.navButton}
-            onClick={() => {
-              if (lowBorder !== 1) {
-                setHighBorder(highBorder - 1);
-                setLowBorder(lowBorder - 1);
-              }
-            }}
-          >
-            {lowBorder === 1 ? "" : <AiOutlineLeft />}
-          </div>
-
-          {loading ? "" : comp}
-
-          <div
-            className={classes.navButton}
-            onClick={() => {
-              if (highBorder > characterStore.totalPageNumber) {
-                return;
-              } else {
-                setHighBorder(highBorder + 1);
-                setLowBorder(lowBorder + 1);
-              }
-            }}
-          >
-            <AiOutlineRight />
-          </div>
-
-          <div
-            className={classes.navButton}
-            onClick={() => {
-              if (
-                highBorder + 5 > characterStore.totalPageNumber &&
-                characterStore.totalPageNumber >= 5
-              ) {
-                setHighBorder(characterStore.totalPageNumber);
-                setLowBorder(characterStore.totalPageNumber - 5);
-              } else if (characterStore.totalPageNumber >= 5) {
-                setHighBorder(highBorder + 5);
-                setLowBorder(lowBorder + 5);
-              }
-            }}
-          >
-            <AiOutlineDoubleRight />
-          </div>
-        </div>
-      )}
+      {!loading && (<div></div>)}
     </div>
   );
 };
